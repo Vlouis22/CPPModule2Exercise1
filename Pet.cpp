7b48c3dee2b7364390f569e5b1e1ded69dfe0aaa
Valery Louis <louisvalery2022@gmail.com>
2025-03-14 15:37:59 -0400

    added Pet class

---
+++ Pet.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Coordinate.cpp"
+using namespace std;
+
+class Pet{
+    private:
+        Coordinate coordinate;
+        int health;
+    
+    public:
+
+        Pet(){
+            coordinate = Coordinate();
+            health = getRandomNumber(90, 100);
+        }
+
+        int getHealth(){
+            return health;
+        }
+
+        void decreaseHealth(){
+            health -= 5;
+            if (health < 20){
+                health = 20;
+            }
+        }
+
+        void moveAround(){
+            int newX = coordinate.getX() + getRandomNumber(-3, 3);
+            int newY = coordinate.getY() + getRandomNumber(-3, 3);
+            coordinate.changeCoordinate(newX, newY);
+        }
+
+        void moveAround(int newX, int newY){
+            coordinate.changeCoordinate(newX, newY);
+        }
+
+        Coordinate getCoordinate(){
+            return coordinate;
+        }
+
+};
