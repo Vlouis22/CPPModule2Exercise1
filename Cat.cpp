5f93477f00b5cae5c4ebadacb393bbc4d2e335bb
Valery Louis <louisvalery2022@gmail.com>
2025-03-14 19:09:27 -0400

    added a count variable to track number of objects of the cat class

--- Cat.cpp
+++ Cat.cpp
@@ -5,20 +5,26 @@ using namespace std;
 class Cat: public Pet{
     private:
         int numberOfKills;
-    
+        static int count;
+
         public:
 
             Cat(){
                 numberOfKills = 0;
+                count++;
             }
 
             int getNumberOfKills(){
                 return numberOfKills;
             };
+
+            int getCount(){
+                return count;
+            }
             
-            double calculateChanceOfWinning(Dog dog){
-                double catPower = getHealth() * (100 + (numberOfKills * 10))/90;
-                double dogPower = dog.getHealth() + dog.getSize() * 2;
+            double calculateChanceOfWinning(Dog* dog){
+                double catPower = getHealth() * (100 + (numberOfKills * 10))/100;
+                double dogPower = dog->getHealth() + dog->getSize() * 4;
 
                 int diff = catPower - dogPower;
                 if(diff > 25){
@@ -30,13 +36,20 @@ class Cat: public Pet{
                 return changeOfWinning;
             }
 
-            bool turnDog(Dog dog){
+            bool turnDog(Dog* dog){
                 double getChanceOfWinning = calculateChanceOfWinning(dog);
                 double randomNum = getRandomNumber(1,100);
                 if (randomNum <= getChanceOfWinning){
+                    dog->died();
                     numberOfKills++;
                     return true;
                 }
+                decreaseHealth();
+                dog->decreaseHealth();
                 return false;
             }
+
+            
 };
+
+int Cat::count = 0; 
