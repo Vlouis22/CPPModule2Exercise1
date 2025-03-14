8fe01994709556a5d6a7849e9c12bc0128c28c31
Valery Louis <louisvalery2022@gmail.com>
2025-03-14 15:39:19 -0400

    in progress

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+#include "Dog.cpp"
+#include "Cat.cpp"
+
+
+int main()
+{
+
+    // change of health will happen in main
+
+
+    
+
+    return 0;
+}
+
